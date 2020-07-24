ecb34924d127830f35f45ad4bffcc0dcd0d22d9e
Sascha Bussian <sachio.kuro88@gmail.com>
2020-07-24 03:02:26 +0200

    cmake

---
+++ build/CMakeFiles/breakout.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Ball.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Ball.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Block.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Block.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Block_Ball.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Block_Ball.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Controller.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Controller.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Game.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Game.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/GameObject.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/GameObject.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/LevelLoader.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/LevelLoader.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Main.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Main.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/MovableGameObject.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/MovableGameObject.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Paddle.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Paddle.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Physics.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Physics.cpp.o"
+  "/home/kuro/Dev/GitHub/Breakout/Breakout/Renderer.cpp" "/home/kuro/Dev/GitHub/Breakout/build/CMakeFiles/breakout.dir/Breakout/Renderer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../Breakout"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/breakout.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/breakout.dir/Breakout/Ball.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Block.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Block_Ball.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Controller.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Game.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/GameObject.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/LevelLoader.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Main.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/MovableGameObject.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Paddle.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Physics.cpp.o"
+  "CMakeFiles/breakout.dir/Breakout/Renderer.cpp.o"
+  "breakout.pdb"
+  "breakout"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
